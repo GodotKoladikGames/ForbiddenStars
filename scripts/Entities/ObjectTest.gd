d4aff758678c4f47a35c4e007baef03526381a79
koladik <koladik@gmail.com>
2026-02-20 20:44:27 +0300

    Object Ork Update

---
+++ scripts/Entities/ObjectTest.gd
@@ -0,0 +1,39 @@
+class_name TestObject extends StaticBody3D
+
+
+func _input_event(
+	camera: Camera3D,
+	event: InputEvent,
+	event_position: Vector3,
+	normal: Vector3,
+	shape_idx: int
+):
+	if OS.has_feature("mobile"):
+		_touchcreen_input_event(camera, event, event_position, normal, shape_idx)
+		return
+	_mouse_input_event(camera, event, event_position, normal, shape_idx)
+
+func _mouse_input_event(
+	_camera: Camera3D,
+	_event: InputEvent,
+	_event_position: Vector3,
+	_normal: Vector3,
+	_shape_idx: int
+):
+	if _event is InputEventMouseButton:
+		print("Mouse click")
+		return
+	print("Another mouse")
+
+func _touchcreen_input_event(
+	_camera: Camera3D,
+	_event: InputEvent,
+	_event_position: Vector3,
+	_normal: Vector3,
+	_shape_idx: int
+):
+	if _event is InputEventScreenTouch:
+		if _event.pressed:
+			print("Touchscreen pressed")
+		else:
+			print("Touchscreen unpressed")
